cb366b3acf6f819855d8720556252598453c1ec1
Reaper <108386291+R00t-Reaper@users.noreply.github.com>
2023-05-19 04:38:26 +0300

    d

--- jinn game/jinn game/Assets/SettingsMenu.cs
+++ jinn game/jinn game/Assets/SettingsMenu.cs
@@ -2,11 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {
     
 
     public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+           resolutions = SetFullscreen.resolution;
+
+            resolutionDropdown.ClearOptions();
+
+            List<string> options = new List<string>();
+
+            int currentResolutionindex = 0;
+
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                string option = resolutions[i].width + "x" + resolutions[i].height;
+                options.Add(option);
+
+                if(resolutions[i].width == Screen.currentResolution.width && 
+                    resolutions[i].height == Screen.currentResolution.)
+                {
+                    
+                    currentResolutionindex = i;
+
+                }
+            }
+
+            resolutionDropdown.addOptions(options);
+            resolutionDropdown.value = currentResolutionindex;
+            resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetVolume(float volume)
     
@@ -14,6 +48,10 @@ public class SettingsMenu : MonoBehaviour
         audioMixer.SetFloat("volume", volume);
     }
 
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
 
 
 }
